cbe94f5619fa22dc06ba99741a686ae90165c4d5
siyibai <siyibai@tencent.com>
2024-11-06 23:20:12 +0800

    init

--- mysql/db_helper/db_helper.go
+++ mysql/db_helper/db_helper.go
@@ -4,6 +4,8 @@ import (
 	"gorm.io/gorm"
 )
 
+const dbConnKeyPrefix string = "DbHelperDbConn:"
+
 type options struct {
 	dbKey string
 }
@@ -31,4 +33,31 @@ func WithDbKey(name string) func(o *options) {
 	return func(o *options) {
 		o.dbKey = name
 	}
+}
+
+// GetDb 获取db连接
+func (hp *DbHelper) GetDb(ctx context.Context) *gorm.DB {
+	v := ctx.Value(dbConnKeyPrefix + hp.getDbKey()) // 优先使用ctx中的db，保证在一个事务内
+	db, ok := v.(*gorm.DB)
+	if ok && db != nil {
+		return db.WithContext(ctx)
+	}
+
+	return hp.db.WithContext(ctx)
+}
+
+func (hp *DbHelper) getDbKey() string {
+	dbKey := ""
+	if hp.opts != nil {
+		dbKey = hp.opts.dbKey
+	}
+	return dbKey
+}
+
+func (hp *DbHelper) withDB(ctx context.Context, db *gorm.DB) context.Context {
+	return context.WithValue(ctx, dbConnKeyPrefix+hp.getDbKey(), db) // set进ctx的时候，带上db的key，防止同个ctx内使用多个db的时候出现覆盖
+}
+
+func (hp *DbHelper) Reset(ctx context.Context) context.Context {
+	return hp.withDB(ctx, nil)
 }
